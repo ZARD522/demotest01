0a01a7ac1c40453769387f219a83cd9ae9d189e5
ZARD <2422177510@qq.com>
2022-09-18 10:41:46 +0800

    添加项目文件。

---
+++ demotest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace HelloWorldApplication
+{
+    /* 类名为 HelloWorld */
+    class HelloWorld
+    {
+        /* main函数 */
+        static void Main(string[] args)
+        {
+            int a = 100;
+            Console.WriteLine(a);
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("The first demo：\"001test\\n\"");
+            Console.ReadKey();
+        }
+
+    }
+}
