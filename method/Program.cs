1a5f6d8f90561788bf89a241750a3ea902025c75
ZARD <2422177510@qq.com>
2022-09-19 19:57:23 +0800

    9.19_test01

---
+++ method/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace method
+{
+    class Program
+    {
+        static int Add(int a,int b)
+        {
+            int c = a + b;
+            return c;
+        }
+
+
+        enum Day
+        {
+            Sun, Mon, tue, Wed, thu, Fri, Sat
+        }
+
+
+        struct Student
+        {
+            public int num;
+            public string name;
+            public int age;
+            public char sex;
+
+
+            public void SetValues(int nu, string na, int ag, char se)
+            {
+                num = nu;
+                name = na;
+                age = ag;
+                sex = se;
+            }
+        }
+
+        
+
+        delegate int Mul(int a,int b);//委托
+
+
+
+        static void Main(string[] args)
+        {
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //int c = Add(a, b);
+            //Console.WriteLine(c);
+
+
+            //Day day = Day.Mon;
+            //Console.WriteLine(day);
+
+
+            Student stu;
+            stu.num = 2022024;
+            stu.name = "ZARD";
+            stu.age = 22;
+            stu.sex = 'M';
+            Console.WriteLine("学号：" + stu.num + "\n姓名：" + stu.name + "\n性别: " + stu.sex + "\n年龄：" + stu.age);
+
+            Student stu1=new Student();
+            stu1.SetValues(2022,"Cooper",21,'F');
+            Console.WriteLine("学号："+stu1.num+"\n姓名："+stu1.name+"\n性别: "+stu1.sex+"\n年龄："+stu1.age);
+
+
+
+            Mul add1;
+            add1 = Add;
+            Console.WriteLine(add1(2,5));
+
+            Console.ReadKey();
+        }
+        
+    }
+}
--- switch/Program.cs
+++ switch/Program.cs
@@ -10,34 +10,53 @@ namespace @switch
     {
         static void Main(string[] args)
         {
-            int day = Convert.ToInt32(Console.ReadLine());
-            switch (day)
+            //int day = Convert.ToInt32(Console.ReadLine());
+            //switch (day)
+            //{
+            //    case 1:
+            //        Console.WriteLine("Monday");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Tuesday");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Wednesday");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("Thursday");
+            //        break;
+            //    case 5:
+            //        Console.WriteLine("Friday");
+            //        break;
+            //    case 6:
+            //        Console.WriteLine("Saturday");
+            //        break;
+            //    case 7:
+            //        Console.WriteLine("Sunday");
+            //        break;
+            //    default:
+            //        Console.WriteLine("输入错误！！！");
+            //        break;
+            //}
+
+
+
+            int[] arr = { 1, 2, 5, 9 ,6,7,8,0,4};
+            for(int i = 0; i < arr.Length; i++)
             {
-                case 1:
-                    Console.WriteLine("Monday");
-                    break;
-                case 2:
-                    Console.WriteLine("Tuesday");
-                    break;
-                case 3:
-                    Console.WriteLine("Wednesday");
-                    break;
-                case 4:
-                    Console.WriteLine("Thursday");
-                    break;
-                case 5:
-                    Console.WriteLine("Friday");
-                    break;
-                case 6:
-                    Console.WriteLine("Saturday");
-                    break;
-                case 7:
-                    Console.WriteLine("Sunday");
-                    break;
-                default:
-                    Console.WriteLine("输入错误！！！");
-                    break;
+                Console.Write(arr[i]+"  ");
             }
+            Console.WriteLine("\n");
+
+
+            int[] ages;
+            ages = new int[] { 1,2,9,8,4,6,5,7,3};
+            for (int i = 0; i < ages.Length; i++)
+            {
+                Console.Write(ages[i]+"  ");
+            }
+
+
             Console.ReadKey();
         }
     }
